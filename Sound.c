eb4481a86914fc8efd9893d1311792b8e2cf024e
Trey Frank Boehm <trey.frank.boehm@gmail.com>
2017-03-21 14:38:39 -0500

    Basic note sequence using blind waits (debugging)

--- Sound.c
+++ Sound.c
@@ -15,6 +15,15 @@
 #include "dac.h"
 #include "tm4c123gh6pm.h"
 
+// 6-bit 64-element sine wave, copy/pasted from Valvano's spreadsheet
+const unsigned short wave[64] = {
+    32,35,38,41,44,47,49,52,54,56,58,59,
+    61,62,62,63,63,63,62,62,61,59,58,56,
+    54,52,49,47,44,41,38,35,32,29,26,23,
+    20,17,15,12,10,8,6,5,3,2,2,1,1,1,2,
+    2,3,5,6,8,10,12,15,17,20,23,26,29
+};
+
 // **************Sound_Init*********************
 // Initialize Systick periodic interrupts
 // Called once, with sound initially off
@@ -24,7 +33,10 @@
 //           Minimum to be determined by YOU
 // Output: none
 void Sound_Init(uint32_t period){
-    DAC_Init();
+    NVIC_ST_CTRL_R &= ~0x05;
+    NVIC_ST_RELOAD_R = 0x00FFFFFF;
+    NVIC_ST_CURRENT_R = 0x0;
+    NVIC_ST_CTRL_R |= 0x05;
 }
 
 
@@ -37,6 +49,13 @@ void Sound_Init(uint32_t period){
 //         input of zero disable sound output
 // Output: none
 void Sound_Play(uint32_t period){
-
+    int i;
+    for (i = 0; i < 64; i++) {
+        DAC_Out(wave[i]);
+        NVIC_ST_CURRENT_R = 0x00FFFFFF;
+        while (NVIC_ST_RELOAD_R - NVIC_ST_CURRENT_R < period) {
+            ;
+        }
+    }
 }
 
--- Sound.h
+++ Sound.h
@@ -10,6 +10,58 @@
 // Header files contain the prototypes for public functions
 // this file explains what the module does
 
+// Macros for the notes (copy/pasted from Valvano's spreadsheet)
+// Period =  80000000/64/Freq=1250000/Freq
+#define C7   597   // 2093 Hz
+#define B6 633   // 1975.5 Hz
+#define Ais6  670   // 1864.7 Hz
+#define A6 710   // 1760 Hz
+#define Gis6  752   // 1661.2 Hz
+#define G6  797   // 1568 Hz
+#define Fis6  845   // 1480 Hz
+#define F6   895   // 1396.9 Hz
+#define E6  948   // 1318.5 Hz
+#define Dis6  1004   // 1244.5 Hz
+#define D6  1064   // 1174.7 Hz
+#define Cis6  1127   // 1108.7 Hz
+#define C6   1194   // 1046.5 Hz
+#define B5    1265   // 987.8 Hz
+#define Ais5   1341   // 932.3 Hz
+#define A5    1420   // 880 Hz
+#define Gis5   1505   // 830.6 Hz
+#define G5  1594   // 784 Hz
+#define Fis5  1689   // 740 Hz
+#define F5  1790   // 698.5 Hz
+#define E5   1896   // 659.3 Hz
+#define Dis5  2009   // 622.3 Hz
+#define D5   2128   // 587.3 Hz
+#define Cis5  2255   // 554.4 Hz
+#define C5  2389   // 523.3 Hz
+#define B4  2531   // 493.9 Hz
+#define Ais4 2681   // 466.2 Hz
+#define A4  2841   // 440 Hz
+#define Gis4 3010   // 415.3 Hz
+#define G4  3189   // 392 Hz
+#define Fis4 3378   // 370 Hz
+#define F4  3579   // 349.2 Hz
+#define E4  3792   // 329.6 Hz
+#define Dis4 4018   // 311.1 Hz
+#define D4  4257   // 293.7 Hz
+#define Cis4 4510   // 277.2 Hz
+#define C4  4778   // 261.6 Hz
+#define B3  5062   // 246.9 Hz
+#define Ais3 5363   // 233.1 Hz
+#define A3  5682   // 220 Hz
+#define Gis3 6020   // 207.7 Hz
+#define G3 6378   // 196 Hz
+#define Fis3 6757   // 185 Hz
+#define F3  7159   // 174.6 Hz
+#define E3  7584   // 164.8 Hz
+#define Dis3 8035   // 155.6 Hz
+#define D3  8513   // 146.8 Hz
+#define Cis3 9019   // 138.6 Hz
+#define C3  9556   // 130.8 Hz
+
 // **************Sound_Init*********************
 // Initialize Systick periodic interrupts
 // Called once, with sound initially off
