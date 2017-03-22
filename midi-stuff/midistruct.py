40d74d0e62799fc94809622c2205788943180364
Trey Frank Boehm <trey.frank.boehm@gmail.com>
2017-03-21 22:47:01 -0500

    Changed script name; requires MIDI as cli arg

--- midi-stuff/midistruct.py
+++ midi-stuff/midistruct.py
@@ -2,17 +2,24 @@
 
 import csv
 import os
+import sys
 
-FILE = "mirrors.csv"
+try:
+    filename = sys.argv[1][0:-5]
+except IndexError:
+    sys.stderr.write("Usage: ./midistruct.py [filename.midi]\n")
+    exit(1)
 
-# Array to associate MIDI pitch numbers with my pitch macros in C
+# Define array to associate MIDI pitch numbers with my pitch macros in C
 macroNumbers=["REST", "Cis0", "D0", "Dis0", "E0", "F0", "Fis0", "G0", "Gis0", "A0", "Ais0", "B0", "C1", "Cis1", "D1", "Dis1", "E1", "F1", "Fis1", "G1", "Gis1", "A1", "Ais1", "B1", "C2", "Cis2", "D2", "Dis2", "E2", "F2", "Fis2", "G2", "Gis2", "A2", "Ais2", "B2", "C3", "Cis3", "D3", "Dis3", "E3", "F3", "Fis3", "G3", "Gis3", "A3", "Ais3", "B3", "C4", "Cis4", "D4", "Dis4", "E4", "F4", "Fis4", "G4", "Gis4", "A4", "Ais4", "B4", "C5", "Cis5", "D5", "Dis5", "E5", "F5", "Fis5", "G5", "Gis5", "A5", "Ais5", "B5", "C6", "Cis6", "D6", "Dis6", "E6", "F6", "Fis6", "G6", "Gis6", "A6", "Ais6", "B6", "C7", "Cis7", "D7", "Dis7", "E7", "F7", "Fis7", "G7", "Gis7", "A7", "Ais7", "B7", "C8", "Cis8", "D8", "Dis8", "E8", "F8", "Fis8", "G8", "Gis8", "A8", "Ais8", "B8", "C9", "Cis9", "D9", "Dis9", "E9", "F9", "Fis9", "G9", "Gis9", "A9", "Ais9", "B9", "C10", "Cis10", "D10", "Dis10", "E10", "F10", "Fis10", "G10"]
 
-# Remove all the extra spaces after commas
-os.system("cat %s | sed -e 's/,\ /,/g' > %s.tmp; mv %s.tmp %s" %
-        (FILE, FILE, FILE, FILE))
+os.system("midicsv %s.midi > %s.csv" % (filename, filename))
 
-midiCSV = list(csv.reader(open(FILE)))
+# Remove all the extra spaces after commas
+os.system("cat %s.csv | sed -e 's/,\ /,/g' > %s.tmp; mv %s.tmp %s.csv" %
+        (filename, filename, filename, filename))
+
+midiCSV = list(csv.reader(open("%s.csv" % filename)))
 # Find the tempo
 tempo = 0
 for row in midiCSV:
