a38f33f3292bf41f6d53ad6d4f87d9b5db9e3760
Trey Frank Boehm <trey.frank.boehm@gmail.com>
2017-05-03 19:04:54 -0500

    Interfaced buttons on Port D

--- Game/Buttons.c
+++ Game/Buttons.c
@@ -5,14 +5,26 @@
  */
  
 #include <stdint.h>
+#include "tm4c123gh6pm.h"
 #include "ST7735.h"
 #include "ADC.h"
 #include "timers.h"
 
 void Buttons_Init(void){
-	
+    SYSCTL_RCGCGPIO_R |= 0x08; // Start the clock on port D
+	while((SYSCTL_RCGCGPIO_R&0x08) == 0) { ; } // Stabilize
+    GPIO_PORTD_CR_R |= 0x07;
+    GPIO_PORTD_DEN_R |= 0x07;
+    GPIO_PORTD_DIR_R &= ~0x07;
 }
 
 uint8_t Buttons_In(void){
-	
+    uint8_t data = GPIO_PORTD_DATA_R & 0x07;
+    if (data & 0x04) { // Leftmost is button 1
+        return 1;
+    } else if (data & 0x02) { // Middle is button 2
+        return 2;
+    } else { // Rightmost is button 3
+        return 3;
+    }
 }
