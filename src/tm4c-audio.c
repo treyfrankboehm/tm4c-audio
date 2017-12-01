f7c503cb96f14fa0eda141cd5aec7d0f3f759fbb
Trey Frank Boehm <trey.frank.boehm@gmail.com>
2017-12-01 17:00:05 -0600

    Better interrupt handling, LEDs, and cleaner main loop

--- src/tm4c-audio.c
+++ src/tm4c-audio.c
@@ -1,6 +1,6 @@
 /* main.c
  * This file initializes and runs the timers that run through the MIDI.
- * Trey Boehm, 2017-04-25
+ * Trey Boehm, 2017-10-01
  * Hardware connections: PB0-PB5 are DAC output bits.
  */
 
@@ -9,8 +9,9 @@
 #include "tm4c123gh6pm.h"
 #include "pll.h"
 #include "dac.h"
-//#include "paradigm.h"
-#include "yyz.h"
+#include "ints.h"
+#include "paradigm.h"
+//#include "yyz.h"
 #include "timers.h"
 #include "custom_types.h"
 #include "driverlib/interrupt.h"
@@ -23,11 +24,6 @@ extern uint16_t Volume_Pointers[4];
 extern uint8_t* Waves[4];
 extern uint8_t* Volumes[4];
 
-bool IntMasterEnable(void); // from driverlib/interrupt.h
-
-//void DisableInterrupts(void);
-//void EnableInterrupts(void);
-
 uint32_t Durations[4]     = {0};
 uint32_t Pitches[4]       = {0};
 uint32_t Event_Lengths[4] = {0};
@@ -37,7 +33,14 @@ const Song *Channels[] = {Channel0, Channel1, Channel2, Channel3};
 
 int main(void) {
     uint8_t i;
-    
+
+    SYSCTL_RCGC2_R |= 0x20;
+    for (i = 0; i < 4; i++) { ; }
+    GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;
+    GPIO_PORTF_DIR_R |= 0x0e;
+    GPIO_PORTF_DEN_R |= 0x0e;
+    GPIO_PORTF_DATA_R |= 0x02;
+ 
     PLL_Init();          // Bus clock at 80 MHz
     DAC_Init();          // Port B outputs DAC voltages 
     Timer0A_Init(A4);    // Timers 0A-3A are for each channel
@@ -50,19 +53,19 @@ int main(void) {
         Pitches[i]   = Channels[i][Event_Indices[i]].pitch;
         Durations[i] = Channels[i][Event_Indices[i]].duration;
     }
-    //EnableInterrupts();  // Each timer has an ISR associated with it
-    IntMasterEnable();
-    
+
+    EnableInterrupts();  // Each timer has an ISR associated with it
+   
+    for (i = 0; i < 4; i++) {
+        Event_Indices[i] = 0;
+        Event_Lengths[i] = 0;
+        MIDI_Time = 0;
+        Tempo_Index = 0;
+        Volume_Pointers[i] = 2;
+        Pitches[i]   = Channels[i][Event_Indices[i]].pitch;
+        Durations[i] = Channels[i][Event_Indices[i]].duration;
+    }
     while (1) {
-        for (i = 0; i < 4; i++) {
-            Event_Indices[i] = 0;
-            Event_Lengths[i] = 0;
-            MIDI_Time = 0;
-            Tempo_Index = 0;
-            Volume_Pointers[i] = 2;
-            Pitches[i]   = Channels[i][Event_Indices[i]].pitch;
-            Durations[i] = Channels[i][Event_Indices[i]].duration;
-        }
         DAC_Out();
     }
 }
